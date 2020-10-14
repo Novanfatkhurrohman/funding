5eae05cd032a55a6f8391631c1169830d1b23d03
novanfatkhurrohman <novanfatkhurrohman9@gmail.com>
2020-10-14 14:02:58 +0700

    update file

---
+++ src/pages/Home.svelte
@@ -0,0 +1,23 @@
+<script>
+  import CharityList from "../components/CharityList.svelte";
+  import { charities } from "../data/charities.js";
+  import Header from "../components/Header.svelte";
+  import Welcome from "../components/Welcome.svelte";
+  import Promo from "../components/Promo.svelte";
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<style>
+  h1 {
+    font-size: 3em;
+    color: brown;
+  }
+</style>
+
+<Header />
+<Welcome />
+
+<CharityList {charities} />
+
+<Promo />
+<Footer />
